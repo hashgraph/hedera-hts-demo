9816ee9e09ea77c926c8504231980d14da102961
Greg Scullard <gregscullard@hedera.com>
2020-11-20 15:28:48 +0100

    Remove commented code

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store/store";
 import vuetify from "./plugins/vuetify";
-// import { EventBus } from "./eventBus"; // path to vuetify export
 
 Vue.config.productionTip = false;
 Vue.config.productionTip = false;
@@ -17,20 +16,3 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount("#app");
-
-// force refresh
-// let interval = setInterval(() => {
-//   void store.dispatch("fetch");
-// }, 2500);
-
-// force refresh
-// EventBus.$on("busy", busy => {
-  // if (busy) {
-  //   clearInterval(interval);
-  // } else {
-  //   void store.dispatch("fetch");
-  //   interval = setInterval(() => {
-  //     void store.dispatch("fetch");
-  //   }, 2500);
-  // }
-// });
