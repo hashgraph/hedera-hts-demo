589b4289e3677829ffe11161ec07b691f03d388e
Greg Scullard <gregscullard@hedera.com>
2020-11-20 15:27:21 +0100

    Tidy up + lint

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -9,151 +9,146 @@
             class="elevation-1"
             hide-default-footer
           >
-
             <template v-slot:item.tokenId="{ item }">
-                {{ item.tokenSymbol }} ({{ item.tokenId }})
+              {{ item.tokenSymbol }} ({{ item.tokenId }})
             </template>
 
             <template v-slot:item.freezeStatus="{ item }">
-              <v-chip
-                  :color="getColor(item.freezeStatus, true)"
-                  dark
-              >
+              <v-chip :color="getColor(item.freezeStatus, true)" dark>
                 {{ item.freezeStatus }}
               </v-chip>
             </template>
 
             <template v-slot:item.kycStatus="{ item }">
-              <v-chip
-                  :color="getColor(item.kycStatus, false)"
-                  dark
-              >
+              <v-chip :color="getColor(item.kycStatus, false)" dark>
                 {{ item.kycStatus }}
               </v-chip>
             </template>
 
             <template v-slot:item.related="{ item }">
-              <v-chip
-                  :color="getColor(item.related, false)"
-                  dark
-              >
+              <v-chip :color="getColor(item.related, false)" dark>
                 {{ item.related }}
               </v-chip>
-              <v-btn v-if="item.related === 'No'" color="blue darken-1" text @click="associate(item.tokenId)">
+              <v-btn
+                v-if="item.related === 'No'"
+                color="blue darken-1"
+                text
+                @click="associate(item.tokenId)"
+              >
                 <v-icon>mdi-link-variant</v-icon>
               </v-btn>
-              <v-btn v-else color="blue darken-1" text @click="dissociate(item.tokenId)">
+              <v-btn
+                v-else
+                color="blue darken-1"
+                text
+                @click="dissociate(item.tokenId)"
+              >
                 <v-icon>mdi-link-variant-off</v-icon>
               </v-btn>
             </template>
           </v-data-table>
         </v-col>
       </v-row>
-      <v-card class="mx-auto ma-4">
-        <v-toolbar color="primary" dark>
-          <v-toolbar-title class="white--text">Transfers and swaps to/from {{ transferTo }}</v-toolbar-title>
-        </v-toolbar>
-        <v-card-text>
-          <v-row>
-            <v-col cols="3">
-              <v-select
+      <v-form ref="form" v-model="valid">
+        <v-card class="mx-auto ma-4">
+          <v-toolbar color="primary" dark>
+            <v-toolbar-title class="white--text"
+              >Transfers and swaps to/from {{ transferTo }}</v-toolbar-title
+            >
+          </v-toolbar>
+          <v-card-text>
+            <v-row>
+              <v-col cols="3">
+                <v-select
                   :items="transferableTokens"
                   label="Token 1"
                   v-model="tokenToTransfer1"
-              ></v-select>
-            </v-col>
-            <v-col cols="5">
-              <v-text-field
+                ></v-select>
+              </v-col>
+              <v-col cols="5">
+                <v-text-field
                   label="Token Quantity* (includes decimals, negative to receive)"
                   :rules="integerRules"
                   v-model="quantityToTransfer1"
                   required
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="3">
-              <v-select
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="3">
+                <v-select
                   :items="transferableTokens"
                   label="Token 2"
                   v-model="tokenToTransfer2"
-              ></v-select>
-            </v-col>
-            <v-col cols="5">
-              <v-text-field
+                ></v-select>
+              </v-col>
+              <v-col cols="5">
+                <v-text-field
                   label="Token Quantity* (includes decimals, negative to receive)"
                   :rules="integerRules"
                   v-model="quantityToTransfer2"
                   required
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="5">
-              <v-text-field
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="3">
+                <v-text-field
                   label="hBar* (negative to send)"
                   :rules="integerRules"
                   v-model="hBars"
                   required
-              ></v-text-field>
-            </v-col>
-          </v-row>
-        </v-card-text>
-        <v-card-actions class="justify-end">
-          <v-btn
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-card-text>
+          <v-card-actions class="justify-end">
+            <v-btn
               text
               color="blue darken-1"
               @click="tokenSwap"
               :disabled="!formValid"
-          >
-            Transfer
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+            >
+              Transfer
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-form>
     </div>
     <div v-else>Wallet account isn't setup</div>
   </v-container>
 </template>
 
 <script>
-import { amountWithDecimals, getAccountDetails } from "../utils";
-import { EventBus } from "../eventBus";
+import { getAccountDetails } from "../utils";
 import { tokenAssociate, tokenDissociate } from "../service/tokenService";
-import {tokenSwap} from "@/service/tokenService";
+import { tokenSwap } from "@/service/tokenService";
+import { EventBus } from "@/eventBus";
 
 export default {
   name: "Wallet",
   props: ["walletInstance"],
   data: function() {
     return {
-      tokens: this.$store.getters.getTokens,
+      valid: false,
       numberOfTokens: this.$store.getters.numberOfTokens,
-      currentToken: this.$store.getters.getTokens[
-        localStorage.getItem(this.walletInstance) || ""
-      ],
       transferTo: "",
       tokenToTransfer1: "",
       quantityToTransfer1: 0,
       tokenToTransfer2: "",
       quantityToTransfer2: 0,
       hBars: 0,
-      accountRelation: undefined,
-      canTransfer: false,
-      balance: 0,
-      hbarBalance: 0,
-      kycStatus: false,
-      freezeStatus: false,
       accountTokens: [],
       transferableTokens: [],
       integerRules: [v => v == parseInt(v) || "Integer required"],
       headers: [
-        { text: 'Token', align: 'center', value: 'tokenId' },
-        { text: 'Associated', align: 'center', value: 'related' },
-        { text: 'hBar Balance', align: 'center', value: 'hbarBalance' },
-        { text: 'token Balance', align: 'center', value: 'tokenBalance' },
-        { text: 'Frozen', align: 'center', value: 'freezeStatus' },
-        { text: 'KYCd', align: 'center', value: 'kycStatus' },
-      ],
+        { text: "Token", align: "center", value: "tokenId" },
+        { text: "Associated", align: "center", value: "related" },
+        { text: "hBar Balance", align: "center", value: "hbarBalance" },
+        { text: "token Balance", align: "center", value: "tokenBalance" },
+        { text: "Frozen", align: "center", value: "freezeStatus" },
+        { text: "KYCd", align: "center", value: "kycStatus" }
+      ]
     };
   },
   computed: {
@@ -162,34 +157,39 @@ export default {
       return account.accountId;
     },
     formValid() {
-      return true;
+      if (
+        this.tokenToTransfer1 !== "" &&
+        !this.quantityToTransfer1 === parseInt(this.quantityToTransfer1)
+      ) {
+        return false;
+      }
+      if (
+        this.tokenToTransfer2 !== "" &&
+        !this.quantityToTransfer2 === parseInt(this.quantityToTransfer2)
+      ) {
+        return false;
+      }
+      if (!this.hBars === parseInt(this.hBars)) {
+        return false;
+      }
+      if (this.tokenToTransfer1 === "" && this.tokenToTransfer2 === "") {
+        return false;
+      }
+      if (
+        parseInt(this.quantityToTransfer1) +
+          parseInt(this.quantityToTransfer2) ===
+        0
+      ) {
+        return false;
+      }
+
+      return this.valid;
     }
   },
   created() {
     this.loadTokenData();
-    const walletToken = localStorage.getItem(this.walletInstance) || undefined;
-    if (typeof walletToken !== "undefined") {
-      this.setCurrentToken(this.tokens[walletToken]);
-    }
     // not clean but can't get VUEX to trigger a watch, this is a quick fix
     this.interval = setInterval(() => {
-      this.tokens = this.$store.getters.getTokens;
-      if (
-        typeof this.tokens !== "undefined" &&
-        typeof this.$store.getters.getAccounts !== "undefined"
-      ) {
-        const accountRelations = this.$store.getters.getAccounts[this.accountId]
-          .tokenRelationships;
-        for (const tokenId in this.tokens) {
-          this.tokens[tokenId].associated =
-            typeof accountRelations[tokenId] !== "undefined";
-        }
-        const walletToken =
-          localStorage.getItem(this.walletInstance) || undefined;
-        if (typeof walletToken !== "undefined") {
-          this.setCurrentToken(this.tokens[walletToken]);
-        }
-      }
       this.loadTokenData();
       this.$forceUpdate();
     }, 1000);
@@ -199,9 +199,9 @@ export default {
   },
   methods: {
     getColor(status, reverseLogic) {
-      if (status === "n/a") return 'grey';
-      else if (status === "Yes") return reverseLogic ? 'red' : 'green';
-      else return reverseLogic ? 'green' : 'red';
+      if (status === "n/a") return "grey";
+      else if (status === "Yes") return reverseLogic ? "red" : "green";
+      else return reverseLogic ? "green" : "red";
     },
     loadTokenData() {
       this.transferableTokens = [];
@@ -214,19 +214,20 @@ export default {
       }
 
       const accountRelations = this.$store.getters.getAccounts[this.accountId]
-          .tokenRelationships;
+        .tokenRelationships;
       // cycle all available tokens
-      for (const tokenId in this.tokens) {
+      const tokens = this.$store.getters.getTokens;
+      for (const tokenId in tokens) {
         const oneToken = {
           tokenId: tokenId,
-          tokenSymbol: this.tokens[tokenId].symbol,
+          tokenSymbol: tokens[tokenId].symbol,
           related: "No",
           hbarBalance: "n/a",
           tokenBalance: "n/a",
           freezeStatus: "n/a",
           kycStatus: "n/a",
           transferable: false
-        }
+        };
         if (typeof accountRelations[tokenId] !== "undefined") {
           oneToken.related = "Yes";
           oneToken.hbarBalance = accountRelations[tokenId].hbarBalance;
@@ -234,15 +235,20 @@ export default {
           if (accountRelations[tokenId].freezeStatus === null) {
             oneToken.freezeStatus = "n/a";
           } else {
-            oneToken.freezeStatus = (accountRelations[tokenId].freezeStatus) ? "Yes" : "No";
+            oneToken.freezeStatus = accountRelations[tokenId].freezeStatus
+              ? "Yes"
+              : "No";
           }
           if (accountRelations[tokenId].kycStatus === null) {
             oneToken.kycStatus = "n/a";
           } else {
-            oneToken.kycStatus = (accountRelations[tokenId].kycStatus) ? "Yes" : "No";
+            oneToken.kycStatus = accountRelations[tokenId].kycStatus
+              ? "Yes"
+              : "No";
           }
         }
-        const otherRelation = this.$store.getters.getAccounts[this.transferTo].tokenRelationships;
+        const otherRelation = this.$store.getters.getAccounts[this.transferTo]
+          .tokenRelationships;
         if (typeof otherRelation[tokenId] !== "undefined") {
           //TODO: Check kyc and freeze status on other account
           oneToken.transferable = true;
@@ -257,70 +263,18 @@ export default {
     dissociate(tokenId) {
       tokenDissociate(tokenId, this.walletInstance);
     },
-    tokenSwap() {
-      tokenSwap(
-          this.walletInstance,
-          this.transferTo,
-          this.tokenToTransfer1,
-          this.quantityToTransfer1,
-          this.tokenToTransfer2,
-          this.quantityToTransfer2,
-          this.hBars
+    async tokenSwap() {
+      EventBus.$emit("busy", true);
+      await tokenSwap(
+        this.walletInstance,
+        this.transferTo,
+        this.tokenToTransfer1,
+        this.quantityToTransfer1,
+        this.tokenToTransfer2,
+        this.quantityToTransfer2,
+        this.hBars
       );
-    },
-    transferToken() {
-      let transferTo = "";
-      if (this.walletInstance === "wallet1") {
-        transferTo = getAccountDetails("wallet2").accountId;
-      } else {
-        transferTo = getAccountDetails("wallet1").accountId;
-      }
-      const transfer = {
-        operation: "transfer",
-        tokenId: this.currentToken.tokenId,
-        fixedDestination: transferTo,
-        user: this.walletInstance
-      };
-      EventBus.$emit("transferDialog", transfer);
-    },
-    selectToken(token) {
-      localStorage.setItem(this.walletInstance, token.tokenId);
-      this.setCurrentToken(token);
-    },
-    setCurrentToken(token) {
-      this.currentToken = token;
-      if (typeof token !== "undefined") {
-        this.accountRelation = this.$store.getters.getAccounts[
-          this.accountId
-        ].tokenRelationships[token.tokenId];
-        if (typeof this.accountRelation !== "undefined") {
-          this.balance = amountWithDecimals(
-            this.accountRelation.balance,
-            this.currentToken.decimals
-          );
-          if (this.accountRelation.kycStatus === null) {
-            this.kycStatus = false;
-          } else {
-            this.kycStatus = this.accountRelation.kycStatus;
-          }
-          if (this.accountRelation.freezeStatus === null) {
-            this.freezeStatus = false;
-          } else {
-            this.freezeStatus = this.accountRelation.freezeStatus;
-          }
-
-          this.canTransfer =
-            this.accountRelation.freezeStatus === null ||
-            (this.kycStatus && !this.freezeStatus);
-        } else {
-          this.canTransfer = false;
-          this.balance = 0;
-        }
-      } else {
-        this.balance = 0;
-        this.accountRelation = undefined;
-        this.canTransfer = false;
-      }
+      EventBus.$emit("busy", false);
     }
   }
 };
