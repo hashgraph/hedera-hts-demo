b0a83098c110de30c4c228e0863001aa1734dd0a
Greg Scullard <gregscullard@hedera.com>
2020-12-09 16:27:53 +0100

    Create file under operator + file chunking

--- src/service/fileService.js
+++ src/service/fileService.js
@@ -1,20 +1,20 @@
-import { hederaClientForUser } from "./client";
-import { notifyError, notifySuccess } from "../utils";
+import { notifyError, notifySuccess} from "../utils";
 import { EventBus } from "@/eventBus";
+import {hederaClient} from "@/service/client";
 
 const {
   FileContentsQuery,
   FileCreateTransaction,
+  FileUpdateTransaction,
+  FileAppendTransaction,
   FileId,
+  PrivateKey,
+  Hbar,
   Status
 } = require("@hashgraph/sdk");
 
-function ownerClient() {
-  return hederaClientForUser("owner");
-}
-
 export async function fileGetContents(fileId) {
-  const client = ownerClient();
+  const client = hederaClient();
   let info = {};
   try {
     info = await new FileContentsQuery()
@@ -27,32 +27,88 @@ export async function fileGetContents(fileId) {
 }
 
 export async function fileCreate(fileData) {
-  const client = ownerClient();
+  const privateKey = PrivateKey.fromString(process.env.VUE_APP_OPERATOR_KEY);
+  const client = hederaClient();
   let fileId = "";
-  // const autoRenewPeriod = 7776000; // set to default 3 months
+  const fileChunk = 4000;
+  const largeFile = fileData.length > fileChunk;
+  let startIndex = 0;
   try {
-    const response = await new FileCreateTransaction()
-        .setContents(fileData)
-        .execute(client);
-    const transactionReceipt = await response.getReceipt(client);
+    const keys = [];
+    keys.push(privateKey);
+    const fileCreateTransaction = new FileCreateTransaction();
+    if (largeFile) {
+      // if we have a large file (> 4000 bytes), create the file with keys
+      // then run file append
+      // then remove keys
+      fileCreateTransaction.setContents(fileData.slice(0, fileChunk));
+      fileCreateTransaction.setKeys(keys)
+    } else {
+      fileCreateTransaction.setContents(fileData);
+    }
+
+    let response = await fileCreateTransaction.setMaxTransactionFee(Hbar.from(10)).execute(client);
+    let transactionReceipt = await response.getReceipt(client);
 
     if (transactionReceipt.status !== Status.Success) {
       notifyError(transactionReceipt.status.toString());
-    } else {
-      fileId = transactionReceipt.fileId.toString();
+      return "";
+    }
 
-      const transaction = {
-        id: response.transactionId.toString(),
-        type: "fileCreate",
-        inputs: fileData.substr(0, 20),
-        outputs: "fileId=" + fileId
-      };
-      EventBus.$emit("addTransaction", transaction);
+    fileId = transactionReceipt.fileId.toString();
 
-      notifySuccess("file created successfully");
+    const transaction = {
+      id: response.transactionId.toString(),
+      type: "fileCreate",
+      inputs: fileData.substr(0, 20),
+      outputs: "fileId=" + fileId,
+    };
+
+    startIndex = startIndex + fileChunk;
+    let chunks = 1;
+    while (startIndex <= fileData.length) {
+      notifySuccess("Saving token properties file chunk " + chunks);
+      chunks += 1;
+      // sleep 500ms to avoid duplicate tx errors
+      await new Promise(r => setTimeout(r, 500));
+      // append to file
+      console.log(startIndex + "-" + (startIndex + fileChunk));
+      response = await new FileAppendTransaction()
+          .setContents(fileData.slice(startIndex, startIndex + fileChunk))
+          .setFileId(FileId.fromString(fileId))
+          .setMaxTransactionFee(Hbar.from(10))
+          .execute(client);
+      let transactionReceipt = await response.getReceipt(client);
+
+      if (transactionReceipt.status !== Status.Success) {
+        notifyError(transactionReceipt.status.toString());
+        return "";
+      }
+      startIndex = startIndex + fileChunk;
+    }
+
+    EventBus.$emit("addTransaction", transaction);
+
+    if (largeFile) {
+      // remove keys
+      response = await new FileUpdateTransaction()
+          .setKeys([])
+          .setFileId(FileId.fromString(fileId))
+          .execute(client);
+      transactionReceipt = await response.getReceipt(client);
+
+      if (transactionReceipt.status !== Status.Success) {
+        notifyError(transactionReceipt.status.toString());
+        return "";
+      }
+
+      notifySuccess("Token properties file created");
     }
   } catch (err) {
     notifyError(err.message);
+    console.error(err);
+
+    return "";
   }
   return fileId;
 }
