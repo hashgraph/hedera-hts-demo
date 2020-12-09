fc019c0996606b176e06cea07744b252feddd054
Greg Scullard <gregscullard@hedera.com>
2020-12-09 16:27:03 +0100

    Add token template

--- src/components/NonFungibleComposer.vue
+++ src/components/NonFungibleComposer.vue
@@ -1,202 +1,244 @@
 <template>
-  <v-stepper v-model="step" alt-labels>
-    <v-stepper-header>
-      <v-stepper-step :complete="step > STEP_NAME" :step="STEP_NAME">
-        Name
-      </v-stepper-step>
-      <v-divider></v-divider>
-      <v-stepper-step :complete="step > STEP_PROPERTIES" :step="STEP_PROPERTIES">
-        Properties
-      </v-stepper-step>
-      <v-divider></v-divider>
-      <v-stepper-step :complete="step > STEP_KYC" :step="STEP_KYC">
-        KYC
-      </v-stepper-step>
-      <v-divider></v-divider>
-      <v-stepper-step
-        :complete="step > STEP_FREEZABLE"
-        :step="STEP_FREEZABLE"
-      >
-        Freezable
-      </v-stepper-step>
-      <v-divider></v-divider>
-      <v-stepper-step :complete="step > STEP_CREATE" :step="STEP_CREATE">
-        Create
-      </v-stepper-step>
-    </v-stepper-header>
+  <v-container>
+    <v-alert type="warning" v-if="photoSize > 4096">
+      This is a large image (size greater than 4kb) - A smaller size is preferable
+    </v-alert>
+    <v-stepper v-model="step" alt-labels>
+      <v-stepper-header>
+        <v-stepper-step :complete="step > STEP_NAME" :step="STEP_NAME">
+          Name
+        </v-stepper-step>
+        <v-divider></v-divider>
+        <v-stepper-step :complete="step > STEP_TEMPLATE" :step="STEP_TEMPLATE">
+          Template
+        </v-stepper-step>
+        <v-divider></v-divider>
+        <v-stepper-step :complete="step > STEP_PROPERTIES" :step="STEP_PROPERTIES">
+          Properties
+        </v-stepper-step>
+        <v-divider></v-divider>
+        <v-stepper-step :complete="step > STEP_KYC" :step="STEP_KYC">
+          KYC
+        </v-stepper-step>
+        <v-divider></v-divider>
+        <v-stepper-step
+          :complete="step > STEP_FREEZABLE"
+          :step="STEP_FREEZABLE"
+        >
+          Freezable
+        </v-stepper-step>
+        <v-divider></v-divider>
+        <v-stepper-step :complete="step > STEP_CREATE" :step="STEP_CREATE">
+          Create
+        </v-stepper-step>
+      </v-stepper-header>
 
-    <v-stepper-items>
-      <v-stepper-content :step="STEP_NAME">
-        <v-card class="mb-12" height="200px" flat>
-          <v-card-text>
-            <v-form ref="nameForm" v-model="nameValid">
-              <v-row>
-                <v-col cols="12">
-                  <v-text-field
-                    label="Name*"
-                    :rules="nameRules"
-                    v-model="name"
-                    required
-                  ></v-text-field>
+      <v-stepper-items>
+        <v-stepper-content :step="STEP_NAME">
+          <v-card class="mb-12" :height=cardHeight flat>
+            <v-card-text>
+              <v-form ref="nameForm" v-model="nameValid">
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      label="Name*"
+                      :rules="nameRules"
+                      v-model="name"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-form>
+            </v-card-text>
+          </v-card>
+          <v-row>
+            <v-col>
+              <v-btn text @click="cancel">
+                Cancel
+              </v-btn>
+            </v-col>
+            <v-spacer></v-spacer>
+            <v-col>
+              <v-btn color="primary" @click="nextStep" :disabled="!nameValid">
+                Continue
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-stepper-content>
+
+        <v-stepper-content :step="STEP_TEMPLATE">
+          <v-card class="mb-12" :height=cardHeight flat>
+            <v-card-text>
+              <v-form ref="templateForm">
+                <v-row>
+                  <v-col cols="12">
+                    <v-select
+                        label="Token Template*"
+                        v-model="tokenTemplate"
+                        :items="tokenTemplatesForSelection"
+                        @change="selectTemplate()"
+                        required
+                    ></v-select>
+                  </v-col>
+                </v-row>
+              </v-form>
+            </v-card-text>
+          </v-card>
+          <v-row>
+            <v-col>
+              <v-btn text @click="cancel">
+                Cancel
+              </v-btn>
+            </v-col>
+            <v-spacer></v-spacer>
+            <v-col>
+              <v-btn text class="mr-2" @click="backStep">
+                Back
+              </v-btn>
+              <v-btn color="primary" @click="nextStep" :disabled="tokenTemplate === ''">
+                Continue
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-stepper-content>
+
+        <v-stepper-content :step="STEP_PROPERTIES">
+          <v-card class="mb-12" :height=cardHeight flat>
+            <v-card-text>
+              <v-row dense>
+                <v-col v-if="schema.properties.photo" cols="8">
+                  <v-form v-model="valid">
+                    <v-jsf v-model="model" :schema="schema"/>
+                  </v-form>
+                </v-col>
+                <v-col v-else cols="12">
+                  <v-form v-model="valid">
+                    <v-jsf v-model="model" :schema="schema"/>
+                  </v-form>
+                </v-col>
+                <v-col v-if="imageData" cols="4">
+                  <img v-if="imageData" :src="imageData" width="100%">
                 </v-col>
               </v-row>
-            </v-form>
-          </v-card-text>
-        </v-card>
-        <v-row>
-          <v-col>
-            <v-btn text @click="cancel">
-              Cancel
-            </v-btn>
-          </v-col>
-          <v-spacer></v-spacer>
-          <v-col>
-            <v-btn text class="mr-2" @click="backStep">
-              Back
-            </v-btn>
-            <v-btn color="primary" @click="nextStep" :disabled="!nameValid">
-              Continue
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-stepper-content>
+            </v-card-text>
+          </v-card>
 
-      <v-stepper-content :step="STEP_PROPERTIES">
-        <v-card class="mb-12" height="200px" flat>
-          <v-card-text>
-            <v-row>
-              <v-col v-if="schema.properties.photo" cols="8">
-                <v-form v-model="valid">
-                  <v-jsf v-model="model" :schema="schema"/>
-                </v-form>
-              </v-col>
-              <v-col v-else cols="12">
-                <v-form v-model="valid">
-                  <v-jsf v-model="model" :schema="schema"/>
-                </v-form>
-              </v-col>
-              <v-col v-if="imageData" cols="4">
-                <img v-if="imageData" :src="imageData" width="100%">
-              </v-col>
-            </v-row>
-          </v-card-text>
-        </v-card>
+          <v-row>
+            <v-col>
+              <v-btn text @click="cancel">
+                Cancel
+              </v-btn>
+            </v-col>
+            <v-spacer></v-spacer>
+            <v-col>
+              <v-btn text class="mr-2" @click="backStep">
+                Back
+              </v-btn>
+              <v-btn color="primary" @click="nextStep">
+                Continue
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-stepper-content>
 
-        <v-row>
-          <v-col>
-            <v-btn text @click="cancel">
-              Cancel
-            </v-btn>
-          </v-col>
-          <v-spacer></v-spacer>
-          <v-col>
-            <v-btn text class="mr-2" @click="backStep">
-              Back
-            </v-btn>
-            <v-btn color="primary" @click="nextStep">
-              Continue
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-stepper-content>
+        <v-stepper-content :step="STEP_KYC">
+          <v-card class="mb-12" :height=cardHeight flat>
+            <v-card-text>
+              <v-radio-group v-model="kyc">
+                <v-radio name="kyc" label="No" value="no"></v-radio>
+                <v-radio name="kyc" label="Yes" value="yes"></v-radio>
+              </v-radio-group>
+            </v-card-text>
+          </v-card>
 
-      <v-stepper-content :step="STEP_KYC">
-        <v-card class="mb-12" height="200px" flat>
-          <v-card-text>
-            <v-radio-group v-model="kyc">
-              <v-radio name="kyc" label="No" value="no"></v-radio>
-              <v-radio name="kyc" label="Yes" value="yes"></v-radio>
-            </v-radio-group>
-          </v-card-text>
-        </v-card>
+          <v-row>
+            <v-col>
+              <v-btn text @click="cancel">
+                Cancel
+              </v-btn>
+            </v-col>
+            <v-spacer></v-spacer>
+            <v-col>
+              <v-btn text class="mr-2" @click="backStep">
+                Back
+              </v-btn>
+              <v-btn color="primary" @click="nextStep">
+                Continue
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-stepper-content>
 
-        <v-row>
-          <v-col>
-            <v-btn text @click="cancel">
-              Cancel
-            </v-btn>
-          </v-col>
-          <v-spacer></v-spacer>
-          <v-col>
-            <v-btn text class="mr-2" @click="backStep">
-              Back
-            </v-btn>
-            <v-btn color="primary" @click="nextStep">
-              Continue
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-stepper-content>
+        <v-stepper-content :step="STEP_FREEZABLE">
+          <v-card class="mb-12" :height=cardHeight flat>
+            <v-card-text>
+              <v-radio-group v-model="freeze">
+                <v-radio name="freeze" label="No" value="no"></v-radio>
+                <v-radio name="freeze" label="Yes" value="yes"></v-radio>
+              </v-radio-group>
+              <v-row>
+                <v-col cols="6">
+                  <v-checkbox
+                    v-model="defaultFreezeStatus"
+                    :disabled="freeze === 'no'"
+                    label="Default"
+                  ></v-checkbox>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
 
-      <v-stepper-content :step="STEP_FREEZABLE">
-        <v-card class="mb-12" height="200px" flat>
-          <v-card-text>
-            <v-radio-group v-model="freeze">
-              <v-radio name="freeze" label="No" value="no"></v-radio>
-              <v-radio name="freeze" label="Yes" value="yes"></v-radio>
-            </v-radio-group>
-            <v-row>
-              <v-col cols="6">
-                <v-checkbox
-                  v-model="defaultFreezeStatus"
-                  :disabled="freeze === 'no'"
-                  label="Default"
-                ></v-checkbox>
-              </v-col>
-            </v-row>
-          </v-card-text>
-        </v-card>
+          <v-row>
+            <v-col>
+              <v-btn text @click="cancel">
+                Cancel
+              </v-btn>
+            </v-col>
+            <v-spacer></v-spacer>
+            <v-col>
+              <v-btn text class="mr-2" @click="backStep">
+                Back
+              </v-btn>
+              <v-btn color="primary" @click="nextStep">
+                Continue
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-stepper-content>
 
-        <v-row>
-          <v-col>
-            <v-btn text @click="cancel">
-              Cancel
-            </v-btn>
-          </v-col>
-          <v-spacer></v-spacer>
-          <v-col>
-            <v-btn text class="mr-2" @click="backStep">
-              Back
-            </v-btn>
-            <v-btn color="primary" @click="nextStep">
-              Continue
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-stepper-content>
+        <v-stepper-content :step="STEP_CREATE">
+          <v-card class="mb-12" :height=cardHeight flat>
+            <v-card-title
+              >You're about to create a non fungible token named
+              {{ name }}</v-card-title
+            >
+            <v-card-text align="left">
+              <P>{{ tokenDetails() }}</P>
+              <P>{{ tokenCompliance() }}</P>
+              <P>It will be immutable and could be used as a deed or title.</P>
+            </v-card-text>
+          </v-card>
 
-      <v-stepper-content :step="STEP_CREATE">
-        <v-card class="mb-12" height="200px" flat>
-          <v-card-title
-            >You're about to create a non fungible token named
-            {{ name }}</v-card-title
-          >
-          <v-card-text align="left">
-            <P>{{ tokenDetails() }}</P>
-            <P>{{ tokenCompliance() }}</P>
-            <P>It will be immutable and could be used as a deed or title.</P>
-          </v-card-text>
-        </v-card>
-
-        <v-row>
-          <v-col>
-            <v-btn text @click="cancel">
-              Cancel
-            </v-btn>
-          </v-col>
-          <v-spacer></v-spacer>
-          <v-col>
-            <v-btn text class="mr-2" @click="backStep">
-              Back
-            </v-btn>
-            <v-btn color="primary" @click="createToken()">
-              Proceed
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-stepper-content>
-    </v-stepper-items>
-  </v-stepper>
+          <v-row>
+            <v-col>
+              <v-btn text @click="cancel">
+                Cancel
+              </v-btn>
+            </v-col>
+            <v-spacer></v-spacer>
+            <v-col>
+              <v-btn text class="mr-2" @click="backStep">
+                Back
+              </v-btn>
+              <v-btn color="primary" @click="createToken()">
+                Proceed
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-stepper-content>
+      </v-stepper-items>
+    </v-stepper>
+  </v-container>
 </template>
 
 <script>
@@ -208,6 +250,7 @@ import VJsf from '@koumoul/vjsf/lib/VJsf.js';
 import '@koumoul/vjsf/lib/VJsf.css';
 import '@koumoul/vjsf/lib/deps/third-party.js';
 import {fileCreate} from "@/service/fileService";
+import {loadTokenTemplates} from "@/service/tokenProperties";
 
 export default {
   name: "NonFungibleComposer",
@@ -215,14 +258,17 @@ export default {
   data: function() {
     return {
       STEP_NAME: 1,
-      STEP_PROPERTIES: 2,
-      STEP_KYC: 3,
-      STEP_FREEZABLE: 4,
-      STEP_CREATE: 5,
+      STEP_TEMPLATE: 2,
+      STEP_PROPERTIES: 3,
+      STEP_KYC: 4,
+      STEP_FREEZABLE: 5,
+      STEP_CREATE: 6,
       nameValid: false,
       step: 1,
-      kyc: "yes",
-      freeze: "yes",
+      kyc: "no",
+      freeze: "no",
+      photoSize: 0,
+      cardHeight: 300,
       nameRules: [
         v => !!v || "Input required",
         v => v.length <= 100 || "Max length 100"
@@ -234,72 +280,19 @@ export default {
       valid: false,
       model: {},
       schema: {
-        type: 'object',
-        properties: {
-          dominantColor: {
-            type: 'string',
-            title: "Dominant Skin Color",
-            enum: [
-                "green", "brown", "red", "blue"
-            ]
-          },
-          otherColor: {
-            type: 'string',
-            title: "Other Skin Color",
-            enum: [
-              "green", "brown", "red", "blue"
-            ]
-          },
-          adultSize: {
-            type: 'string',
-            title: "Adult size",
-            enum: [
-              "tiny", "small", "medium", "large"
-            ]
-          },
-          eyeShape: {
-            type: 'string',
-            title: "Shape of my eyes",
-            enum: [
-              "oval", "round", "square"
-            ]
-          },
-          dangerLevel: {
-            type: 'string',
-            title: "Am I dangerous to humans",
-            enum: [
-              "a bit slimy", "poisonous", "lethal"
-            ]
-          },
-          photo: {
-            type: "object",
-            title: "photo",
-            contentMediaType: "image/*",
-            writeOnly: true,
-            properties: {
-              type: {
-                type: "string"
-              },
-              data: {
-                type: "string"
-              }
-            }
-          },
-          },
-        }
-      }
+        type: "object",
+        properties: {}
+      },
+      tokenTemplates: {},
+      tokenTemplate: "",
+      tokenTemplatesForSelection: [],
+    }
   },
   created() {
-    EventBus.$on("tokenCompose", () => {
-      this.nameValid = false;
-      this.kyc = "yes";
-      this.freeze = "yes";
-      this.step = 1;
-      //
-      this.name = "";
-      this.symbol = "";
-      this.defaultFreezeStatus = false;
-    })
+    this.init();
+    EventBus.$on("tokenCompose",() => {
+      this.init();
+    });
   },
   computed: {
     imageData() {
@@ -307,7 +300,36 @@ export default {
     }
   },
   methods: {
+    init() {
+      this.nameValid = false;
+      this.kyc = "no";
+      this.freeze = "no";
+      this.step = 1;
+      //
+      this.name = "";
+      this.symbol = "";
+      this.defaultFreezeStatus = false;
+      this.template = "";
+      this.photoSize = 0;
+      this.tokenTemplates = loadTokenTemplates();
+      this.tokenTemplatesForSelection = [];
+      for (const templateItem in this.tokenTemplates) {
+        this.tokenTemplatesForSelection.push(templateItem);
+      }
+    },
+    selectTemplate() {
+      this.model = {};
+      if (this.tokenTemplate === "") {
+        this.schema = {
+          type: "object",
+              properties: {}
+        };
+      } else {
+        this.schema = this.tokenTemplates[this.tokenTemplate];
+      }
+    },
     imageBase64() {
+      this.photoSize = 0;
       if (typeof this.model.photo === "undefined") {
         return undefined;
       }
@@ -317,6 +339,7 @@ export default {
       if (typeof this.model.photo.data === "undefined") {
         return undefined;
       }
+      this.photoSize = this.model.photo.size;
       return "data:" + this.model.photo.type + ";base64," + this.model.photo.data;
     },
     nextStep() {
@@ -326,7 +349,7 @@ export default {
       this.step = this.step - 1;
     },
     cancel() {
-      EventBus.$emit("busy", false);
+      EventBus.$emit("dialogClose");
     },
     async createToken() {
       EventBus.$emit("busy", true);
@@ -364,6 +387,7 @@ export default {
         const newToken = await tokenCreate(token);
         if (typeof newToken.tokenId !== "undefined") {
           this.$store.commit("setToken", newToken);
+          EventBus.$emit("dialogClose");
         }
       }
 
