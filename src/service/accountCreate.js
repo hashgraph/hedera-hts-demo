2cf555e282ae1fe3f21c44a557c6706404535378
Greg Scullard <gregscullard@hedera.com>
2021-01-07 14:06:10 +0100

    Add try / catch to account create

--- src/service/accountCreate.js
+++ src/service/accountCreate.js
@@ -1,5 +1,6 @@
 import { hederaClient } from "./client";
 import { EventBus } from "@/eventBus";
+import {notifyError} from "@/utils";
 
 const {
   PrivateKey,
@@ -10,30 +11,37 @@ const {
 export async function accountCreate(wallet) {
   const client = hederaClient();
 
-  const privateKey = await PrivateKey.generate();
+  try {
 
-  const response = await new AccountCreateTransaction()
-    .setKey(privateKey.publicKey)
-    .setInitialBalance(new Hbar(process.env.VUE_APP_INITIAL_BALANCE))
-    .execute(client);
+    const privateKey = await PrivateKey.generate();
 
-  const transactionReceipt = await response.getReceipt(client);
-  const newAccountId = transactionReceipt.accountId;
+    const response = await new AccountCreateTransaction()
+      .setKey(privateKey.publicKey)
+      .setInitialBalance(new Hbar(process.env.VUE_APP_INITIAL_BALANCE))
+      .execute(client);
 
-  const transaction = {
-    id: response.transactionId.toString(),
-    type: "cryptoCreate",
-    inputs: "initialBalance=" + process.env.VUE_APP_INITIAL_BALANCE,
-    outputs: "accountId=" + newAccountId.toString()
-  };
-  EventBus.$emit("addTransaction", transaction);
+    const transactionReceipt = await response.getReceipt(client);
+    const newAccountId = transactionReceipt.accountId;
 
-  return {
-    accountId: newAccountId.toString(),
-    account: {
-      wallet: wallet,
-      privateKey: privateKey.toString(),
-      tokenRelationships: {}
-    }
-  };
+    const transaction = {
+      id: response.transactionId.toString(),
+      type: "cryptoCreate",
+      inputs: "initialBalance=" + process.env.VUE_APP_INITIAL_BALANCE,
+      outputs: "accountId=" + newAccountId.toString()
+    };
+    EventBus.$emit("addTransaction", transaction);
+
+    return {
+      accountId: newAccountId.toString(),
+      account: {
+        wallet: wallet,
+        privateKey: privateKey.toString(),
+        tokenRelationships: {}
+      }
+    };
+  } catch (err) {
+    notifyError(err.message);
+    console.error(err);
+    return {};
+  }
 }
