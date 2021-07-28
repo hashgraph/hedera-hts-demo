27eb37d6b12211456ad836918ab244abe8472535
Serg Metelin <sergey.metelin@hedera.com>
2021-07-28 04:53:55 -0400

    Gitpod integration (#17)
    * Added gipod basic setup
    
    * Gitpod configuration to disable host check
    
    * vue.config.js gitpod setup
    
    * - Branch Gitpod integration
    - Implement app halt if no Hedera credentials found in the environmental variables
    
    * - Implement better error popup on missing testnet credentials
    
    * - Wrapped .env in the error message with a code tag for formating purposes

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   transpileDependencies: ["vuetify", "@koumoul/vjsf"],
   configureWebpack: {
-    devtool: "source-map"
+    devtool: "source-map",
+    devServer: {
+      host: '0.0.0.0',
+      allowedHosts: ['localhost', '.gitpod.io'],
+    }
   }
 };
